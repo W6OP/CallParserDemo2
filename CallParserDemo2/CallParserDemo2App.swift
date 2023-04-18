a6f14fcb9f0e8661616e1078041e0ba8a2fc5490
Peter Bourget <pbourget@w6op.com>
2023-04-18 08:58:00 -0700

    Initial Commit

---
+++ CallParserDemo2/CallParserDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  CallParserDemo2App.swift
+//  CallParserDemo2
+//
+//  Created by Peter Bourget on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CallParserDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
